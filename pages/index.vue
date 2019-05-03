3c47199e37d5cade0e1c2282e1fd087aed31ec0a
Aaron Gerig <a.gerig@w-vision.ch>
2019-05-03 16:55:47 +0200

    add command for Heroku

--- pages/index.vue
+++ pages/index.vue
@@ -9,16 +9,14 @@
         My bedazzling Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
+        <nuxt-link
+          v-for="post in posts"
+          :to="{ name: 'posts-id', params: { id: post.id } }"
+          :key="post.id"
           class="button--grey"
-        >GitHub</a>
+        >
+         {{ post.title }}
+        </nuxt-link>
       </div>
     </div>
   </section>
@@ -30,14 +28,30 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+
+  head () {
+    return {
+      title: 'Home Page 🍕',
+      meta: [
+        { name: 'twitter:title', content: 'Nuxt Fundamentals by Vue School' },
+        { name: 'twitter:description', content: 'Nuxt + Vue School = 🍕' },
+        { name: 'twitter:image', content: 'https://i.imgur.com/UYP2umJ.png' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+      ]
+    };
+  },
+
+  computed: {
+    posts () {
+      return this.$store.state.posts.all;
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -46,11 +60,10 @@ export default {
 
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
-  color: #35495e;
   letter-spacing: 1px;
 }
 
@@ -59,7 +72,6 @@ export default {
   font-size: 42px;
   color: #526488;
   word-spacing: 5px;
-  padding-bottom: 15px;
 }
 
 .links {
